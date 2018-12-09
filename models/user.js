8dcec980c0270f27846a9ffce4f521a126e43745
Anton Bondarev <anton.v.bondarev@gmail.com>
2018-12-09 20:28:40 +0300

    done auth splitting

--- models/user.js
+++ models/user.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  name: { type: String },
-  age: { type: Number },
-  occupation: { type: String },
-  city: { type: String },
-  admin: { type: Boolean }
-}, {
-    collection: 'users'
-  });
+const userSchema = new mongoose.Schema(
+	{
+		email: { type: String },
+		password: { type: String },
+		name: { type: String },
+		age: { type: Number },
+		occupation: { type: String },
+		city: { type: String },
+		admin: { type: Boolean }
+	},
+	{
+		collection: 'users'
+	}
+);
 
 const User = mongoose.model('User', userSchema);
 
