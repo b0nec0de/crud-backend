4189211d258219f00d2ac0596dea7009c0f4eb58
Anton Bondarev <a.bondarev@wellyes.ru>
2018-12-11 11:18:56 +0300

    done main functional

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
 	{
 		email: { type: String, required: true },
-		password: { type: String },
+		password: { type: String, required: true },
 		name: { type: String },
 		age: { type: Number },
 		occupation: { type: String },
